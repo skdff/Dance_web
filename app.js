2b607746c3d89cb2e8a0e018316c8e6de5cee0f7
skdff <siddhantk151@gmail.com>
2026-09-24 05:35:05 +0530

    Backend updated

--- app.js
+++ app.js
@@ -2,7 +2,33 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+const bodyparser = require("body-parser");
 const port = 3000;
+//Mongoose-DB Conection setup
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
+  console.log("MongoDB connected");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+//Defining the mongoose contact schema
+const contactSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  email: String,
+  address: String,
+  desc: String
+
+});
+
+//Compiling the model
+const Contact = mongoose.model('Contact', contactSchema);
 
 //EXPRESS SPECIFIC STUFF
 app.use('/static',express.static('static'));//For Serving static files
@@ -27,6 +53,30 @@ app.get('/contact',(req, res)=>{
     const params = {}
     res.status(200).render('contact.pug',params)
 })
+//Getting the Post request
+app.post('/contact', (req, res) => {
+
+    var myData = new Contact(req.body);
+
+    myData.save().then(() => {
+
+        res.send(`
+            <h2>The Data Item is saved Successfully</h2>
+
+            <script>
+                setTimeout(() => {
+                    window.location.href = "/";
+                }, 3000);
+            </script>
+        `);
+
+    }).catch(() => {
+
+        res.status(400).send("Data Item is not Saved Successfully");
+
+    });
+
+});
 
 //START THE SERVER
 app.listen(port, () => {
